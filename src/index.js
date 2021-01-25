a519f4d4a615e3b402b88c046ade799405954703
Konstantin <fandeluk.k@gmail.com>
2021-01-25 23:27:08 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === null || matrix === undefined || matrix.length <= 0)
+        return [];
+    let a = matrix.length;
+    let res = [];
+    for (let i = 0; i < a; i++) {
+        res = res.concat(i % 2 == 0 ? matrix[i] : matrix[i].reverse());
+    }
+    return res;
+};
